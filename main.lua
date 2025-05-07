ade0f8fc6b6f25f759060f6c491fd5f1e5b5f9ed
kirk-en <kirkenbysk@gmail.com>
2025-05-07 15:05:06 -0400

    built hand disable effect

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: Pizza Arcade
 --- MOD_ID: PizzaArcade
---- MOD_AUTHOR: [Kirk, Bryce]
---- MOD_DESCRIPTION: An example mod on how to create Jokers.
+--- MOD_AUTHOR: [Kirk Enbysk, Bryce Doerksen]
+--- MOD_DESCRIPTION: A vanilla-style Joker expansion that delivers a greasy slice of 90s pizza arcade nostalgia. Expect tokens, tickets, animatronic eeriness, and a possible appearence from a legally distinct rodent. Jokers were all coded on a CRT monitor. Play responsibly—no tokens required.
 --- PREFIX: pizza
 ----------------------------------------------
 ------------MOD CODE -------------------------
+
+SMODS.Atlas {
+  key = "modicon",
+  path = "mod_icon.png",
+  px = 34,
+  py = 34,
+}
 
 SMODS.Atlas{
   key = "Jokers",
@@ -16,15 +23,65 @@ SMODS.Atlas{
 
 SMODS.Joker{
   key = 'bryce',
+  atlas = 'Jokers',
+  pos = {x = 0, y = 0},
+  unlocked = true,
+  discovered = true,
+  blueprint_compat = true,
+  eternal_compat = true,
+  perishable_compat = true,
+  rarity = 1,
+  cost = 7,
+  config = {extra = {mult = 13}},
   loc_txt = {
     name = 'bryce the D!',
     text = {
-      '{C:attention}+12{} mult',
+      '{C:green}+13{} mult',
       'An ok joker for newbies.'
     }
   },
+  calculate = function (self,card,context )
+    if context.joker_main then
+      return {
+        mult = card.ability.extra.mult
+      }
+    end
+  end
+}
+
+SMODS.Joker{
+  key = 'slug',
   atlas = 'Jokers',
-  pos = {x = 0, y = 0}
+  pos = {x = 2, y = 0},
+  unlocked = true,
+  discovered = true,
+  blueprint_compat = true,
+  eternal_compat = true,
+  perishable_compat = true,
+  rarity = 2,
+  cost = 1,
+  config = {extra = {mult = 20}},
+  loc_txt = {
+    name = 'bryce the D!',
+    text = {
+      '{C:green}+20{} mult',
+      '1 in 5 chance of losing remaining hands this round'
+    }
+  },
+  calculate = function (self,card,context )
+    if context.joker_main then
+      return {
+        mult = card.ability.extra.mult
+      }
+    end
+    if context.after then
+      -- if math.random(5) == 1 then  -- 1 in 5 chance
+      -- card:destroy()
+      G.GAME.hands['Four of a Kind'].mult = 0
+      print ('this is hands in round:', G.GAME.hands['Four of a Kind'])
+    -- end
+  end
+end
 }
 
 ----------------------------------------------
---
+++ readme.md
@@ -0,0 +1,3 @@
+# 🍕 Pizza Arcade Jokers - A Balatro Mod
+
+A vanilla-style Joker expansion that delivers a greasy slice of 90s pizza arcade nostalgia. Expect tokens, tickets, animatronic eeriness, and a possible appearance from a legally distinct rodent. Jokers were all coded on a CRT monitor. Play responsibly—no tokens required.
